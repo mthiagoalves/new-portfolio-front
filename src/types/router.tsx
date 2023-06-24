01f2cfecfad128fc23d18b91ceb0fab624ac1c9a
mthiagoalves <m.thiago.ales@live.com>
2023-06-24 12:18:55 +0100

    feat: add config to router;

---
+++ src/types/router.tsx
@@ -0,0 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import { RoutePath } from "./routes";
+
+const Router = () => {
+    return (
+        <Routes>
+            <Route path={RoutePath.HOME} element={</Home>}>
+
+            </Route>
+        </Routes>
+    );
+}
+
+export default Router;
---
+++ src/types/routes.ts
@@ -0,0 +1,9 @@
+export enum RoutePath {
+    HOME = '/',
+    LOGIN = '/login',
+    SETTINGS = '/seetings',
+    SEETINGS_PROJECTS = 'projects',
+    SEETINGS_TECHNOLOGIES = 'technologies',
+    SEETINGS_USERS = 'users'
+
+}
