ad5191537a7beda0b2e69244e71c7e069e850254
mthiagoalves <m.thiago.alves@live.com>
2023-06-26 10:27:54 +0100

    chore: add ReactQuery

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { QueryClientProvider, QueryClient } from '@tanstack/react-query';
 import { ThemeProvider } from 'styled-components';
 import theme from 'assets/styles/theme';
 import { BrowserRouter } from "react-router-dom";
 import Router from 'types/router';
 import './App.css';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
    <ThemeProvider theme={theme}>
     <BrowserRouter>
-      <Router/>
+      <QueryClientProvider client={queryClient}>
+        <Router/>
+      </QueryClientProvider>
     </BrowserRouter>
    </ThemeProvider>
   );
