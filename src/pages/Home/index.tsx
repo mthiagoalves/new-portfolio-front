4f58abce3c0fb9389a724da50bada666607a7f91
mthiagoalves <m.thiago.alves@live.com>
2023-06-26 10:37:12 +0100

    feat(home): created componenet home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,5 @@
+const Home = () => {
+    return(<div>Component Home</div>)
+}
+
+export default Home;
--- src/types/router.tsx
+++ src/types/router.tsx
@@ -1,10 +1,11 @@
 import { Routes, Route } from "react-router-dom";
+import Home from "pages/Home";
 import { RoutePath } from "./routes";
 
 const Router = () => {
     return (
         <Routes>
-            <Route path={RoutePath.HOME} element={</Home>}>
+            <Route path={RoutePath.HOME} element={<Home/>}>
 
             </Route>
         </Routes>
