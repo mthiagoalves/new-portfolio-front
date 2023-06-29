9903d2e24b57972edb7686c9cd3ff3adac11f4a5
mthiagoalves <m.thiago.alves@live.com>
2023-06-29 17:15:56 +0100

    feat(objectives): finished section 'objectives' with responsive mobile

--- src/components/Objectives/index.tsx
+++ src/components/Objectives/index.tsx
@@ -1,14 +1,47 @@
 import * as S from './style';
+import programming from 'assets/images/programming.jpg'
 
 const Objectives = () => {
-    return(
-        <S.SecondSection className='text-center'>
-            <S.TitleSection>
-                About me
-            </S.TitleSection>
-            <S.TextSection>
-                A little about me and my way of working.
-            </S.TextSection>
+    return (
+        <S.SecondSection>
+            <S.DivTitleSection>
+                <S.TitleSection>
+                    About me
+                </S.TitleSection>
+                <S.TextSection>
+                    A little about me and my way of working.
+                </S.TextSection>
+            </S.DivTitleSection>
+            <div className="container-fluid p-0">
+                <div className="row m-0">
+                    <div className="col-5 text-center d-none d-sm-block">
+                        <S.ImgSection src={programming} alt="Image i programming"/>
+                    </div>
+                    <div className="col-sm-7 col-12 p-0">
+                        <S.TextSection>
+                            It can be said that I am a very curious person. I like to do a lot of research to be able to solve certain things, and I try as much as possible to update myself and the projects I'm working on. I always try to keep time in my favor, I take the issue of quality very much into account and charge myself a lot about it, but always attentive to deadlines. I am currently working in Portugal at Covet Group where I am responsible for some of the group's websites. Where I do maintenance, implementation of new features, SQL, implementation of new projects. 
+                        </S.TextSection>
+                        <S.LabelLanguages>
+                            English:
+                        </S.LabelLanguages>
+                        <S.ProgressBar>
+                            <S.ProgressBarInner>
+                                <S.ProgressBarInnerFilled  style={{ width: '35%' }}></S.ProgressBarInnerFilled>
+                                <S.ProgressBarInnerLabel>100%</S.ProgressBarInnerLabel>
+                            </S.ProgressBarInner>
+                        </S.ProgressBar>
+                        <S.LabelLanguages>
+                            Portuguese:
+                        </S.LabelLanguages>
+                        <S.ProgressBar>
+                            <S.ProgressBarInner>
+                                <S.ProgressBarInnerFilled  style={{ width: '100%' }}></S.ProgressBarInnerFilled>
+                                <S.ProgressBarInnerLabel>Native</S.ProgressBarInnerLabel>
+                            </S.ProgressBarInner>
+                        </S.ProgressBar>
+                    </div>
+                </div>
+            </div>
         </S.SecondSection>
     );
 }
--- src/components/Objectives/style.ts
+++ src/components/Objectives/style.ts
@@ -3,8 +3,10 @@ import styled, { css } from "styled-components";
 export const SecondSection = styled.div`
     ${({theme}) => css`
         background-color: ${theme.colors.bgColorWhite};
-        min-height: 100vh;
         padding: 3rem 6.25rem;
+        @media(max-width: 767px){
+            padding: 3rem 2.25rem;
+        }
     `}
 `;
 
@@ -37,8 +39,70 @@ export const TitleSection = styled.h2`
 export const TextSection = styled.p`
     ${({theme}) => css`
         display: inline-block;
-        font-size: 1.2rem;
+        font-size: ${theme.constants.bodyTextFontSize};
         margin: 1rem 0;
         color: ${theme.colors.textColorDark};
+        text-align: justify;
+    `}
+`;
+
+export const ImgSection = styled.img`
+    ${() => css`
+        max-width: 70%;
+        height: auto;
+        margin: 0 auto;
+    `}
+`;
+
+export const ProgressBar = styled.div`
+    ${({theme}) => css`
+        position: relative;
+        width: 100%;
+        height: 1.25rem;
+        background-color: ${theme.colors.bgColorDark};
+        border-radius: 0.625rem;
+    `}
+`;
+
+export const ProgressBarInner = styled.div`
+    ${() => css`
+        position: relative;
+        height: 100%;
+        border-radius: 0.625rem;
+    `}
+`;
+
+export const ProgressBarInnerFilled = styled.div`
+    ${({theme}) => css`
+        position: absolute;
+        top: 0;
+        left: 0;
+        height: 100%;
+        background-color: ${theme.colors.bgColorGreen};
+        border-radius: 0.625rem;
+        display: flex;
+        align-items: center;
+        padding-left: 0.625rem;
+        padding-bottom: .18rem;
+    `}
+`;
+
+export const ProgressBarInnerLabel = styled.div`
+    ${({theme}) => css`
+        position: absolute;
+        top: 50%;
+        right: 0.313rem;
+        transform: translateY(-54%);
+        font-size: 0.875rem;
+        font-weight: bold;
+        color: ${theme.colors.textColorWhite};
+    `}
+`;
+
+export const LabelLanguages = styled.label`
+    ${({theme}) => css`
+        color: ${theme.colors.textColorDark};
+        font-size: ${theme.colors.bodyTextFontSize};
+        margin-bottom: 0.2rem;
     `}
 `;
