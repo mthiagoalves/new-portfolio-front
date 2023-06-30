7e94ad7173a561c215e043894be2c1e51a329ff7
mthiagoalves <m.thiago.alves@live.com>
2023-06-30 14:54:42 +0100

    feat: create component 'knowledges' & 'cardKnowledges'

---
+++ src/components/Knowledges/index.tsx
@@ -0,0 +1,18 @@
+import * as S from './style';
+
+const Knowledges = () => {
+    return (
+        <S.ThirdSection>
+            <S.DivTitleSection>
+                <S.TitleSection>
+                    Knowledges
+                </S.TitleSection>
+                <S.TextSection>
+                    Here i will show on the technologies that I have knowledge of.
+                </S.TextSection>
+            </S.DivTitleSection>
+        </S.ThirdSection>
+    );
+}
+
+export default Knowledges;
---
+++ src/components/Knowledges/style.ts
@@ -0,0 +1,55 @@
+import styled, { css } from "styled-components";
+
+export const ThirdSection = styled.div`
+    ${({theme}) => css`
+        background-color: ${theme.colors.bgColorDark};
+        padding: 3rem 6.25rem;
+        @media(max-width: 767px){
+            padding: 3rem 2.25rem;
+        }
+    `}
+`;
+
+export const DivTitleSection = styled.div`
+    ${() => css`
+        text-align:center;
+    `}
+`;
+
+export const TitleSection = styled.h2`
+    ${({theme}) => css`
+        position: relative;
+        color: ${theme.colors.textColorWhite};
+        font-size: ${theme.constants.bodyTitleFontSize};
+        font-weight: 700;
+        letter-spacing: 0.063rem;
+        &::before{
+            content: "";
+            position: absolute;
+            left: 50%;
+            transform: translateX(-50%);
+            bottom: -0.8rem;
+            width: 3.75rem;
+            height: 0.25rem;
+            background: ${theme.colors.bgColorWhite}
+        }
+    `}
+`;
+
+export const TextSection = styled.p`
+    ${({theme}) => css`
+        display: inline-block;
+        font-size: ${theme.constants.bodyTextFontSize};
+        margin: 1rem 0;
+        color: ${theme.colors.textColorWhite};
+        text-align: justify;
+    `}
+`;
+
+export const ImgSection = styled.img`
+    ${() => css`
+        max-width: 70%;
+        height: auto;
+        margin: 1.6rem;
+    `}
+`;
