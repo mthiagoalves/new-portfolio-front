d10beddbec3005267d3273bcb9d62396b6be9553
mthiagoalves <m.thiago.alves@live.com>
2023-06-30 14:47:36 +0100

    feat(home): import new component 'Knowledges'

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,6 +2,7 @@ import * as S from './style';
 import About from 'components/About';
 import Objectives from 'components/Objectives';
 import Sidebar from 'components/Sidebar';
+import Knowledges from 'components/Knowledges';
 
 const Home = () => {
   return (
@@ -12,7 +13,12 @@ const Home = () => {
         </header>
         <section>
           <About />
+        </section>
+        <section>
           <Objectives />
+        </section>
+        <section>
+          <Knowledges />
         </section>
       </S.HomeContent>
     </S.Home>
