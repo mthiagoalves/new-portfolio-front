2f3e1ddc9d0b3bf6f19b85fdfae632680abfefc8
mthiagoalves <m.thiago.alves@live.com>
2023-07-06 18:18:06 +0100

    feat(modalcomponent): finshed modal customization

--- src/components/ModalProject/index.tsx
+++ src/components/ModalProject/index.tsx
@@ -1,4 +1,7 @@
 import * as S from './style';
+import Github from 'assets/images/projects/icons/github.png';
+import Deploy from 'assets/images/projects/icons/deploy.png';
+import Close from 'assets/images/close.gif';
 
 interface ModalProjectProps {
     title: string,
@@ -9,11 +12,22 @@ interface ModalProjectProps {
 const ModalProject: React.FC<ModalProjectProps> = ({ title, description, technologies }) => {
     return (
         <div>
-            <S.Modal>
+            <S.Modal closing={true}>
                 <S.Content>
                     <h2>{title}</h2>
                     <p>{description}</p>
-                    <p>{technologies}</p>
+                    <p>Technologies: {technologies}</p>
+                    <S.DivIcons>
+                        <a href="/">
+                            <S.CardImg src={Github} alt='Repository' title='Repository' />
+                        </a>
+                        <a href="/">
+                            <S.CardImg src={Deploy} alt='Deploy' title='Deploy' />
+                        </a>
+                    </S.DivIcons>
+                    <S.BtnClose>
+                        <S.CloseImg src={Close} alt='Close' title='Close' />
+                    </S.BtnClose>
                 </S.Content>
             </S.Modal>
         </div>
--- src/components/ModalProject/style.ts
+++ src/components/ModalProject/style.ts
@@ -1,7 +1,17 @@
-import styled, { css } from "styled-components";
+import styled, { css, keyframes } from "styled-components";
 
-export const Modal = styled.div`
-    ${() => css`
+
+const ModifyVisibilityModal = {
+    closing: () => css`
+      opacity: 1;
+      visibility: visible;
+    `,
+  };
+  
+type ModalStyled = { closing: boolean };
+
+export const Modal = styled.div<ModalStyled>`
+    ${({ closing }) => css`
         position: fixed;
         top: 0;
         left: 0;
@@ -11,15 +21,55 @@ export const Modal = styled.div`
         display: flex;
         justify-content: center;
         align-items: center;
-        opacity: 1;
-        visibility: visible;
+        opacity: 0;
+        visibility: hidden;
+        ${closing && ModifyVisibilityModal.closing()};
         transition: opacity 0.3s ease-in-out, visibility 0.3s ease-in-out;
     `}
 `;
 
 export const Content = styled.div`
-    background-color: white;
-    padding: 20px;
-    width: 70%;
+    ${({theme}) => css`
+        position: relative; 
+        background-color: ${theme.colors.bgColorWhite};
+        color: ${theme.colors.textColorDark};
+        padding: 20px;
+        max-width: 70%;
+        text-align: justify;
+        text-align-last: center;
+    `}
+`;
+
+export const DivIcons = styled.div`
+  ${() => css`
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    margin-top: 1rem;
+  `}
+`;
+
+export const CardImg = styled.img`
+  ${() => css` 
+    max-width: 30px;
+    height: auto;
+    margin: 0 50px;
+  `}
+`;
+
+export const CloseImg = styled.img`
+  ${() => css` 
+    max-width: 30px;
+    height: auto;
+  `}
+`;
+
+export const BtnClose = styled.div`
+    ${() => css`
+        position: absolute;
+        top: 0.313rem;
+        right: 0.313rem;
+        cursor: pointer;
+    `}
 `;
 
