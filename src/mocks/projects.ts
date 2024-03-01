4da6698a9f3f900e1187b08c9d90fe8ffbf21330
mthiagoalves <m.thiago.alves@live.com>
2024-03-01 10:08:58 +0000

    feat(projects): add news projects

--- src/mocks/projects.ts
+++ src/mocks/projects.ts
@@ -1,13 +1,33 @@
 export const projects = [
     {
+        "title": "Website Clinic Rampani",
+        "slug": "clinic-rampani",
+        "smallDescription": "Website created to Clinic Rampani",
+        "description": "Developing all website using Laravel, Vue, Tailwind, PHP, and JavaScript was a structured journey, starting from planning to implementation. I carefully defined the requirements, considering my target audience, desired content, and visual design. I set up the Laravel environment using Composer and installed Livewire to facilitate the development of dynamic components. I built the frontend using Bootstrap for a modern and responsive design, integrating it into my Vue components and configuring JavaScript as needed. I created routes and controllers to direct the flow of my application, opting for data modeling when necessary for dynamic information. I developed pages and components in Laravel, using Blade views and Livewire components for dynamic areas such as the display of projects. Integration with the backend (PHP) was achieved through controllers to retrieve data from the database, implementing PHP logic as needed.",
+        "technologies": "PHP, Laravel, Vue, JavaScript, Tailwind",
+        "repository": "https://github.com/mthiagoalves/rampani-project",
+        "deploy": "https://clinicarampani.com.br",
+        "order": 1
+    },
+    {
         "title": "Portfolio Alexandre",
         "slug": "portfolio-alexandre",
         "smallDescription": "Portfolio maked to my friend Alexandre...",
-        "description": "Developing my portfolio using Laravel, Livewire, Bootstrap, PHP, and JavaScript was a structured journey, starting from planning to implementation. I carefully defined the requirements, considering my target audience, desired content, and visual design. I set up the Laravel environment using Composer and installed Livewire to facilitate the development of dynamic components. I built the frontend using Bootstrap for a modern and responsive design, integrating it into my Blade files and configuring JavaScript as needed. I created routes and controllers to direct the flow of my application, opting for data modeling when necessary for dynamic information. I developed pages and components in Laravel, using Blade views and Livewire components for dynamic areas such as the display of projects. Integration with the backend (PHP) was achieved through controllers to retrieve data from the database, implementing PHP logic as needed.",
-        "technologies": "PHP, Laravel, Liveware, JavaScript",
+        "description": "Developing portfolio using Laravel, Livewire, Bootstrap, PHP, and JavaScript was a structured journey, starting from planning to implementation. I carefully defined the requirements, considering my target audience, desired content, and visual design. I set up the Laravel environment using Composer and installed Livewire to facilitate the development of dynamic components. I built the frontend using Bootstrap for a modern and responsive design, integrating it into my Blade files and configuring JavaScript as needed. I created routes and controllers to direct the flow of my application, opting for data modeling when necessary for dynamic information. I developed pages and components in Laravel, using Blade views and Livewire components for dynamic areas such as the display of projects. Integration with the backend (PHP) was achieved through controllers to retrieve data from the database, implementing PHP logic as needed.",
+        "technologies": "PHP, Laravel, Liveware, JavaScript, Botstrap",
         "repository": "https://github.com/mthiagoalves/portfolio-alexandre",
-        "deploy": "In deployment",
-        "order": 1
+        "deploy": "https://alexandrepiedade.com",
+        "order": 2
+    },
+    {
+        "title": "Portfolio Juliana",
+        "slug": "portfolio-juliana",
+        "smallDescription": "Portfolio maked to Juliana Rampani...",
+        "description": "Developing portfolio using Laravel, Livewire, Bootstrap, PHP, and JavaScript was a structured journey, starting from planning to implementation. I carefully defined the requirements, considering my target audience, desired content, and visual design. I set up the Laravel environment using Composer and installed Livewire to facilitate the development of dynamic components. I built the frontend using Bootstrap for a modern and responsive design, integrating it into my Blade files and configuring JavaScript as needed. I created routes and controllers to direct the flow of my application, opting for data modeling when necessary for dynamic information. I developed pages and components in Laravel, using Blade views and Livewire components for dynamic areas such as the display of projects. Integration with the backend (PHP) was achieved through controllers to retrieve data from the database, implementing PHP logic as needed.",
+        "technologies": "PHP, Laravel, Liveware, JavaScript, Botstrap",
+        "repository": "https://github.com/mthiagoalves/portifolio-dra-juliana-rampani",
+        "deploy": "https://clinicarampani.com.br/juliana-rampani",
+        "order": 3
     },
     {
         "title": "Full Backoffice",
@@ -17,7 +37,17 @@ export const projects = [
         "technologies": "PHP, JavaScript, Laravel, Bootstrap, Blade",
         "repository": "https://github.com/mthiagoalves/",
         "deploy": "https://www.circu.net/dashboard/login",
-        "order": 2
+        "order": 4
+    },
+    {
+        "title": "Refactoring Frontend",
+        "slug": "circu-frontend",
+        "smallDescription": "Project created for Circu of the Covet Group...",
+        "description": "<b>1 - Evaluation and Analysis:</b> I initiated the process by conducting a comprehensive analysis of the existing website. I reviewed all front-end elements such as design, layout, navigation, usability, and performance. I also understood the objectives of the redesign, whether it was to enhance user experience, update the appearance, or add new features. <b>2 - Definition of Objectives:</b> Based on the analysis, I clearly defined the objectives of the front-end redesign. For example, objectives included improving usability, making the site mobile-responsive, updating the design for a modern look, or increasing loading speed. <b>Design and Prototyping:</b> I collaborated with the UI/UX team for the design of the new front-end. This involved creating wireframes and prototypes to visualize how the new site would look. I worked with graphic designers to develop an attractive and modern visual that aligned with the redesign goals. <b>4 - Front-End Development:</b> I began coding the new front-end based on the approved design and prototypes. I utilized technologies such as Bootstrap, HTML5, CSS3, and JavaScript to create a responsive and high-performance site. I ensured that the code was optimized for SEO. <b>5 - Testing and Debugging:</b> Rigorous testing was conducted across different browsers and devices to ensure the site functioned correctly and provided a consistent user experience. I addressed any bugs or compatibility issues that arose during testing. <b>6 - Performance Optimization:</b> I optimized the site's performance by minimizing loading times, compressing images, and reducing resource usage. This was crucial to ensure the site was fast and responsive. <b>7 - Content and Data Migration:</b> I transferred existing content to the new site, ensuring that all relevant information was retained. I ensured URLs were redirected appropriately to avoid link breaks. <b>8 - Usability Testing:</b> Real-user usability tests were conducted to evaluate the effectiveness of the implemented usability improvements during the redesign. Feedback was collected, and adjustments were made as necessary. <b>9- Launch and Monitoring:</b> After the successful completion of the redesign, I launched the new front-end. I monitored the site post-launch to ensure everything worked as planned and that the redesign objectives were achieved. <b>10 - Continuous Maintenance and Improvement:</b> Understanding that front-end redesign is an ongoing process, I continued regular maintenance and considered future enhancements as the site's and users' needs evolved.",
+        "technologies": "JavaScript, PHP, React, Laravel",
+        "repository": "https://github.com/mthiagoalves/",
+        "deploy": "https://www.circu.net",
+        "order": 5
     },
     {
         "title": "Refactoring Frontend",
@@ -27,7 +57,7 @@ export const projects = [
         "technologies": "JavaScript, PHP, React, Laravel",
         "repository": "https://github.com/mthiagoalves/",
         "deploy": "https://www.caffelattehome.com",
-        "order": 3
+        "order": 6
     },
     {
         "title": "Backend Buy Cart",
@@ -37,7 +67,7 @@ export const projects = [
         "technologies": "JavaScript, TypeScript, NestJs, PostgreSQL",
         "repository": "https://github.com/mthiagoalves/test-consort-group",
         "deploy": "https://github.com/mthiagoalves/test-consort-group",
-        "order": 4
+        "order": 7
     },
     {
         "title": "Restaurant Orders*Backend",
@@ -47,7 +77,7 @@ export const projects = [
         "technologies": "PHP, Laravel",
         "repository": "https://github.com/mthiagoalves/restaurant_API",
         "deploy": "https://github.com/mthiagoalves/restaurant_API",
-        "order": 1
+        "order": 8
     },
     {
         "title": "New implements Backoffice",
@@ -55,28 +85,8 @@ export const projects = [
         "smallDescription": "New features implemented in Maison Valentina's of the Cover Group",
         "description": "<b>1. Definition of Goals and Requirements:</b> I initiated the project by creating a clear list of goals and requirements for the back office. This included identifying essential functionalities such as product management, order processing, customer management, inventory tracking, and sales analytics. <b>2. Data Structure:</b> I developed a robust database schema to store all necessary information. I configured tables for products, customers, orders, order history, inventory, and more. I used the MySQL database management system to ensure efficiency and scalability. <b>3. Back-End Development:</b> I created the back-end logic for the system, choosing Laravel to develop the required APIs. This allowed seamless communication between the front-end and the back office. Functionalities included the ability to add, edit, and delete products, manage orders, track inventory, and more. <b>4. Security:</b> I implemented rigorous security measures to protect sensitive data from customers and the company. This involved user authentication, role-based authorization, and data encryption. <b>5. Front-End Development:</b> I developed the front-end using Laravel's Blade as requested by the company. I created an intuitive and user-friendly interface for the team to easily manage products, orders, and customers. I also included charts and reports for sales analysis. <b>6. Testing and Debugging:</b> I conducted extensive testing to ensure all functionalities worked seamlessly. Any bugs discovered were addressed before the launch. <b>7. Hosting and Deployment:</b> I selected a reliable hosting platform and deployed the back office. I ensured that all security settings were configured correctly. <b>8. Team Training:</b> I trained the team that would use the back office, ensuring they were well-prepared to effectively use all functionalities. <b>9. Maintenance and Continuous Improvement:</b> After the launch, I continued monitoring the system, fixing bugs, applying security updates, and implementing improvements based on team and user feedback. <i>The successful completion of this back-office creation project for the e-commerce website was crucial for the efficiency and success of the online business.",
         "technologies": "JavaScript, PHP, Laravel, Bootstrap, Swiper",
-        "repository": "github.com",
-        "deploy": "www",
-        "order": 1
+        "repository": "https://github.com/mthiagoalves/",
+        "deploy": "https://www.maisonvalentina.net/en/",
+        "order": 9
     },
-    // {
-    //     "title": "Backoffice 7",
-    //     "slug": "backoffice-7",
-    //     "smallDescription": "Project created for Covet Group...",
-    //     "description": "Project created for Covet Group, where i make a backoffice using Laravel with...",
-    //     "technologies": "JavaScript, PHP, React, Laravel",
-    //     "repository": "github.com",
-    //     "deploy": "www",
-    //     "order": 1
-    // },
-    // {
-    //     "title": "Backoffice 8",
-    //     "slug": "backoffice-8",
-    //     "smallDescription": "Project created for Covet Group...",
-    //     "description": "Project created for Covet Group, where i make a backoffice using Laravel with...",
-    //     "technologies": "JavaScript, PHP, React, Laravel",
-    //     "repository": "github.com",
-    //     "deploy": "www",
-    //     "order": 1
-    // }
 ];
